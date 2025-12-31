fe5160a634b615a54d37368110ba7efd5b41b35f
Rehtt <dsreshiram@gmail.com>
2025-12-31 08:56:56 +0800

    feat: 添加 gitsigns.nvim 插件配置，支持当前行 Git 信息显示

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true, -- 永久开启当前行 Git 信息
+    current_line_blame_opts = {
+      delay = 500, -- 延迟 0.5 秒显示（可自行调整，例如 100 或 1000）
+    },
+  },
+}
