4187191583c6c82cda24de13cb4d5ba4b265003b
Rehtt <dsreshiram@gmail.com>
2026-01-07 16:43:05 +0800

    feat: 为 nvim-lspconfig 添加内联提示配置选项

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    -- 关闭内联提示
+    -- 使用快捷键 <S>uh可以打开
+    inlay_hints = { enabled = false },
     servers = {
       gopls = {
         settings = {
