2c341d1234ac34e711608aa1354f3311074d8ab9
Rehtt <dsreshiram@gmail.com>
2026-01-24 09:21:32 +0800

    feat: 为 snacks.nvim 添加 iTerm 图片支持配置

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,11 @@ return {
     "folke/snacks.nvim",
     opts = {
       notifier = { enabled = true },
+      image = {
+        -- 如果环境变量是 iTerm.app，则强制开启图片支持
+        enabled = os.getenv("TERM_PROGRAM") == "iTerm.app" or os.getenv("LC_TERMINAL") == "iTerm2",
+        force = true, -- 即使是在 tmux 中也尝试透传
+      },
       picker = {
         sources = {
           explorer = {
