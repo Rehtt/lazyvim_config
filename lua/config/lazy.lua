0733ecb4abee56fe415e22b04b39ebe36601e18f
Rehtt <dsreshiram@gmail.com>
2026-08-22 15:35:30 +0800

    feat: 添加 REST 插件支持

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.supermaven" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.sql" },
+    { import = "lazyvim.plugins.extras.util.rest" },
     { import = "plugins" },
   },
   defaults = {
